3b710c19910a47c7cfd91acc5e665b0f5f1e69f1
Aaryan Singh <139672521+aaryanrana01@users.noreply.github.com>
2026-09-06 10:34:45 +0000

    Add frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+// app.get('/', (req, res) => {
+//     res.send("Server is ready");
+// });
+
 app.get('/', (req, res) => {
     res.send("Server is ready");
 });
